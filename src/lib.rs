cea945d3ef39e457c19ab963bdaa0dd50c18e57f
join3r <join3r@gmail.com>
2021-01-19 08:30:22 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use std::{io, time::SystemTime};
+
+pub use anyhow::{anyhow, Context, Result};
+
+/// Holds the values for Human
+pub struct Human {
+    name: String,
+    sex: char,
+    birth_day: u8,
+    birth_month: u8,
+    birth_year: u16,
+}
+
+impl Human {
+    /// Create new instance of Human by getting the relevant information from stdin.
+    /// Can fail if the sex is not entered correctly or birth day, month or year are not numbers
+    pub fn new() -> Result<Self> {
+        let human = Human {
+            name: get_read_line("Zadaj meno:")?,
+            sex: get_read_line("Zadaj pohlavie (M/Z):")?.parse()?, // TODO: Move to Into trait
+            birth_day: get_read_line("Zadaj den narodania")? // FIXME: rozdielny format
+                .parse()
+                .context("Zly format")?, // TODO: Move Error into generic function get_read_line()
+            birth_month: get_read_line("Zadaj mesiac narodenia")?
+                .parse()
+                .context("Zly format")?,
+            birth_year: get_read_line("Zadaj rok narodenia")?
+                .parse()
+                .context("Zly format")?,
+        };
+        // current year from EPOCH time
+        let curr_year = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_secs()
+            / 31556926 // calculates year
+            + 1970; // since EPOCH started in 1970
+        if curr_year - (human.birth_year as u64) < 18 {
+            return Err(anyhow!("Neplnolety, nemozeme pokracovat"));
+        }
+        Ok(human)
+    }
+    /// Converts the fields name, sex, birth_day, birth_month, birth_year to comma separated String
+    pub fn to_csv(&self) -> String {
+        format!(
+            "{},{},{},{},{}\n",
+            self.name, self.sex, self.birth_day, self.birth_month, self.birth_year
+        )
+    }
+}
+
+/// Shows the prompt with a new newline and then requests user input from stdin.
+fn get_read_line(prompt: &str) -> Result<String> { // TODO: Make generic
+    println!("{}", prompt);
+    let mut buff = String::new();
+    io::stdin().read_line(&mut buff).unwrap();
+    Ok(buff.trim().to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,12 @@
-// Gejza bol na join3rovom kurze rustu a po 40 hodinách naprogramoval toto
-// program funguje takmer ako chceme, ale ako vidíte je škaredý
-// vašou úlohou je upraviť program tak, aby bola radosť ho čítať a upravovať
-// Zopár pointerov (haha), ktoré Gejza nenasledoval, ale vy by ste mali
-// KISS - keep it simple stupid
-// DRY - do not repeat yourself
+mod lib;
 
-fn main()
-{
-  println!("Zadaj meno:");
-    let mut a = String::new();
-  std::io::stdin().read_line(&mut a);
-  println!("Zadaj pohlavie (M/Z):");
-  let mut b = String::new();
-  std::io::stdin().read_line(&mut b);if b.trim()=="M"||b.trim()=="m"||b.trim()=="Z"||b.trim()=="z"{
-  println!("Zadaj den narodania");
-  let mut c = String::new();
-  std::io::stdin().read_line(&mut c);
-      let x: i32 = c.trim().parse().unwrap();
-       println!("Zadaj mesiac narodenia");
-       let mut d = String::new();       
-       std::io::stdin().read_line(&mut d);
-      let y: i32 = d.trim().parse().unwrap();
-       println!("Zadaj rok narodenia");
-       let mut input = String::new();
-      std::io::stdin().read_line(&mut input);
-      let z: i32 = input.trim().parse().unwrap();
-      if 2021 - z <= 18 {
-            println!("Neplnolety, nemozeme pokracovat"); } else
-             {
-               let year = 2021 - 18;
-          let a = a.trim();
-           let s = b.trim();
-           let d = c.trim();
-            let f = d.trim();
-           let (g, y) = (input.trim(), y.to_string());
-           let y = y.to_string();
-           let g = vec![a,s,d,&y,g];
-           let a: String = g.iter().map(|a | a.trim()).fold(String::new(), |p,a| format!("{},{}", p.trim(), a.trim()));
-           std::fs::write("datafile.csv", a);
-           println!("ok");  }}    else
-    {
-        println!("Nespravny udaj");}}
+use std::fs::write;
+
+use lib::Human;
+use lib::Result;
+
+fn main() -> Result<()> {
+    let human = Human::new()?;
+    write("datafile.csv", human.to_csv())?;
+    Ok(())
+}
