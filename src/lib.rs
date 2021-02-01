e487721c7136e80afb8f8d9d32cbb87022a26ea1
join3r <join3r@gmail.com>
2021-02-01 19:36:47 +0100

    closures

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io, str::FromStr, time::SystemTime};
+use std::{convert::TryInto, io, str::FromStr, time::SystemTime};
 
 pub use anyhow::{anyhow, Context, Result};
 
@@ -15,22 +15,39 @@ impl Human {
     /// Create new instance of Human by getting the relevant information from stdin.
     /// Can fail if the sex is not entered correctly or birth day, month or year are not numbers
     pub fn new() -> Result<Self> {
-        let human = Human {
-            name: get_read_line("Zadaj meno:", &|meno: String| meno.split_whitespace().count() > 1)?,
-            sex: get_read_line("Zadaj pohlavie (M/Z):")?, // TODO: Move to Into trait
-            birth_day: get_read_line("Zadaj den narodania") // FIXME: rozdielny format
-                .context("Zly format")?, // TODO: Move Error into generic function get_read_line()
-            birth_month: get_read_line("Zadaj mesiac narodenia")
-                .context("Zly format")?,
-            birth_year: get_read_line("Zadaj rok narodenia")
-                .context("Zly format")?,
-        };
-        // current year from EPOCH time
         let curr_year = SystemTime::now() // TODO: calculate real age with chrono
             .duration_since(SystemTime::UNIX_EPOCH)?
             .as_secs()
             / 31556926 // calculates year
             + 1970; // since EPOCH started in 1970
+        let human = Human {
+            name: get_read_line(
+                "Zadaj meno:",
+                &|meno: &String| meno.split_whitespace().count() >= 2,
+                "Musíš zadať aj prvé aj druhé meno",
+            ),
+            sex: get_read_line(
+                "Zadaj pohlavie (M/Z):",
+                &|pohlavie: &char| *pohlavie == 'M' || *pohlavie == 'Z',
+                "Musíš zadať veľké M alebo Z",
+            ),
+            birth_day: get_read_line(
+                "Zadaj den narodania",
+                &|den: &u16| *den <= 31 && *den >= 1,
+                "Deň musí byť medzi 1 a 31",
+            ),
+            birth_month: get_read_line(
+                "Zadaj mesiac narodenia",
+                &|mesiac: &u16| *mesiac <= 12 && *mesiac >= 1,
+                "Mesiac musí byť medzi 1 a 12",
+            ),
+            birth_year: get_read_line(
+                "Zadaj rok narodenia",
+                &|rok: &u16| *rok > 1900 && *rok <= curr_year.try_into().unwrap(),
+                &format!("Rok musí byť medzi 1901 a {}", curr_year),
+            ),
+        };
+        // current year from EPOCH time
         if curr_year - (human.birth_year as u64) < 18 {
             return Err(anyhow!("Neplnolety, nemozeme pokracovat"));
         }
@@ -46,18 +63,28 @@ impl Human {
 }
 
 /// Shows the prompt with a new newline and then requests user input from stdin.
-// String, char, u8
-// 
-fn get_read_line<T, U>(prompt: &str, check: U) -> Result<T>
+
+// get_read_line::<String>(prompt: &str, check: &dyn Fn(&T) -> bool) -> String
+// get_read_line::<u16>(prompt: &str, check: &dyn Fn(&T) -> bool) -> y16
+// get_read_line::<har>(prompt: &str, check: &dyn Fn(&T) -> bool) -> char
+
+fn get_read_line<T>(prompt: &str, check: &dyn Fn(&T) -> bool, err_msg: &str) -> T
 where
     T: FromStr,
     <T as FromStr>::Err: Sync,
     <T as FromStr>::Err: Send,
     <T as FromStr>::Err: std::error::Error + 'static,
-    U: Fn(T) -> bool,
 {
-    println!("{}", prompt);
-    let mut buff = String::new();
-    io::stdin().read_line(&mut buff).unwrap();
-    Ok(buff.trim().parse()?)
+    loop {
+        println!("{}", prompt);
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).unwrap();
+        let buff = buff.trim().parse(); // Ok(T), Err(T)
+        if let Ok(buff) = buff {
+            if check(&buff) {
+                return buff;
+            }
+        }
+        println!("{}\n", err_msg);
+    }
 }
